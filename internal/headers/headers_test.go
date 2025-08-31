0e31a6c7ab02774d6f267a95ec208602225894f6
Ellis Sinclair-Kent <ellissinclairkent@gmail.com>
2025-08-31 06:42:56 +0100

    Ch6L1

---
+++ internal/headers/headers_test.go
@@ -0,0 +1,62 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidSingleHeader(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 23, n)
+	assert.True(t, done)
+}
+
+func TestInvalidSpacingHeader(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+}
+
+func TestValidSingleHeaderWithWhitespace(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("Host:           localhost:42069          \r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 43, n)
+	assert.True(t, done)
+}
+
+func TestValidTwoHeadersWithExistingHeaders(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\nContent: example\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "example", headers["Content"])
+	assert.Equal(t, 41, n)
+	assert.True(t, done)
+}
+
+func TestInvalidDone(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+}
